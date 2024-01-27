95f6f5ec3553ea52f2a3efb38a5948a60ebb4b0e
Zhengxing Gu <z5346386@ad.unsw.edu.au>
2024-01-27 19:42:26 +0800

    fix a bug

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -61,13 +61,13 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="about image"/>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             I am a Master of Information Technology student from University of New South Wales. 
             I have experience working with React, Javascript, HTML, CSS, Python, Pytorch, C/C++, PostgreSQL and Git.
-            I am a quick learner and I'm always looking forward to expand my knowledge and skill sets.
+            I am a quick learner and I am always looking forward to expand my knowledge and skill sets.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
